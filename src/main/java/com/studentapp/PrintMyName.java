817c0847bc7b9e68556df3ab20cb425cbead5731
prmcsep103 <primecommunity103@gmail.com>
2024-07-20 15:22:01 +0100

    freature branch created

---
+++ src/main/java/com/studentapp/PrintMyName.java
@@ -0,0 +1,11 @@
+package com.studentapp;
+
+/**
+ * Created by Jay Vaghani
+ */
+public class PrintMyName {
+
+    public static void main(String[] args) {
+        System.out.println("Jay Vaghani");
+    }
+}
