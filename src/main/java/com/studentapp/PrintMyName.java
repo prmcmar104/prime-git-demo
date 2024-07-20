0d2ac6181cdc565294ae2ad9b3587c8d1d30453b
prmcsep103 <primecommunity103@gmail.com>
2024-07-20 15:29:34 +0100

    change name and surname in two diff line

--- src/main/java/com/studentapp/PrintMyName.java
+++ src/main/java/com/studentapp/PrintMyName.java
@@ -6,6 +6,7 @@ package com.studentapp;
 public class PrintMyName {
 
     public static void main(String[] args) {
-        System.out.println("Jay Vaghani");
+        System.out.println("Jay");
+        System.out.println("Vaghani");
     }
 }
